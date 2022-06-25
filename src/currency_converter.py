af4a3745ca33cfd5775e458910a5ea13755baf03
Netanel86 <iting86@gmail.com>
2022-06-25 22:29:17 +0300

    implemented convert and currency validation functions, handled validation error

--- src/currency_converter.py
+++ src/currency_converter.py
@@ -7,12 +7,15 @@ class Currency:
         self.code = code
         self.country = country
         self.name = name
-        self.rate = rate
+        self.rate = float(rate)
+    
+    def __str__(self):
+        return "[{0}]: Name: {1}, Code: {2}, Rate: {3}".format(self.country, self.name, self.code, self.rate)
 
 class Converter: 
     def __init__(self):
-        self.endpoint = "http://www.boi.org.il/currency.xml?rdate={year}{month}{day}"
-        self.currency_dict = {}
+        self.__endpoint = "http://www.boi.org.il/currency.xml?rdate={year}{month}{day}"
+        self.__currency_dict = {}
 
         response = requests.get(self.__formatUrl())
 
@@ -21,30 +24,52 @@ class Converter:
     def __formatUrl(self):
         today = datetime.datetime.now()
         
-        return self.endpoint.format(
+        return self.__endpoint.format(
             year = today.strftime("%Y"), 
             month = today.strftime("%m"), 
             day = self.__get_last_business_day(today))
 
     def __get_last_business_day(self, today):
-        
+       
         # Lambda function to check the weekday and retrun the days past since last business day
         get_delta = lambda date : 1 if int(date.strftime('%w')) == 5 else 2 if int(date.strftime('%w')) == 6 else 0
-        
-        # fix_day = lambda date : int(date.strftime("%d")) - get_delta(date) if get_delta(date) != 0 else 0
-        # return fix_day(today)
+
         return int(today.strftime("%d")) - get_delta(today)
 
     def __init_currencies(self, xml_response):
         data = xmltodict.parse(xml_response.content)
 
         for currency in data['CURRENCIES']['CURRENCY']:
-            newCurrency = Currency(currency['CURRENCYCODE'],currency['COUNTRY'], currency['NAME'], currency['RATE'])
+            new_currency = Currency(currency['CURRENCYCODE'], currency['COUNTRY'], currency['NAME'], currency['RATE'])
 
-            self.currency_dict[newCurrency.code] = newCurrency
+            self.__currency_dict[new_currency.code] = new_currency
+
+        self.__currency_dict['ILS'] = Currency('ILS', 'Israel', 'New Shekels', 1)
+
+    def __check_currency(self, currency, code):
+        if currency == 0:
+            raise ConversionError("Conversion Error: '{0}' is not a know currency code".format(code))    
+    
+    def convert(self, sum, from_code, to_code):
+        src_currency = self.__currency_dict.get(from_code, 0)
+        target_currency = self.__currency_dict.get(to_code, 0)
+        
+        self.__check_currency(src_currency, from_code)
+        self.__check_currency(target_currency, to_code)
+
+        if from_code == to_code:
+            converted = sum
+        else: 
+            converted = (sum * src_currency.rate) / target_currency.rate
+        
+        return {'converted_sum': converted, 
+            'src_currency': src_currency, 
+            'target_currency': target_currency}
 
     def print_currencies(self):
-        print(self.currency_dict)
-
+        for currency in self.__currency_dict.values():
+            print(currency)
     
-
+class ConversionError(Exception):
+    def __init__(self, *args: object):
+        super().__init__(*args)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,28 @@
 import requests
-from currency_converter import Converter
+from currency_converter import ConversionError, Converter
 
 try:
     converter = Converter()
+    print("My console currency conversion app:")
+    from_str = input("Enter Source Currency:")
+    to_str = input("Enter Target Currency:")
+    sum = input("Enter Amount:")
+    # from_str = 'ILS'
+    # to_str = 'ILS'
+    # sum = 120
+    converted = converter.convert(sum, from_str, to_str)
+
+    print("Converted {0} {1} {2} to {3} {4} {5}".format(sum, 
+        converted['src_currency'].country, 
+        converted['src_currency'].name, 
+        converted['converted_sum'],
+        converted['target_currency'].country,
+        converted['target_currency'].name))
+
+#Catch exception in case of a connection error    
 except requests.exceptions.RequestException as e:
     print("Connection Error:",e)
-else:
-    converter.print_currencies()
+
+#Catch exception in case of a conversion error
+except ConversionError as e:
+    print(e)
